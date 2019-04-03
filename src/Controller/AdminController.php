784acb72e4b9e01cb8be6e9cd0e5a5f850799c23
Dimaxwash <dimaxwash@gmail.com>
2019-04-03 13:44:24 +0200

    version test

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -7,26 +7,17 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class InstrumentController extends AbstractController
+class AdminController extends AbstractController
 {
     /**
-     * @Route("/instruments", name="instruments.table")
+     * @Route("/admin/table", name="admin.table")
      */
     public function table(InstrumentRepository $instrumentRepository):Response
     {
         
         $produits= $instrumentRepository->findAll();
-        return $this->render('instrument/instruments.html.twig', [
+        return $this->render('admin/index.html.twig', [
             'produits' => $produits
         ]);
     }
-    
-    /**
-     * @Route("/", name="home.index")
-     */
-    public function index():Response
-    {
-        return $this->render('instrument/index.html.twig');
-    }
 }
-
